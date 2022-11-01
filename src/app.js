e499099bce7ea5230b6b877196abd2d7d1f0b834
Alex Ivanov <alex.ivanov@unity3d.com>
2022-11-02 00:15:05 +0200

    Snake head

--- src/app.js
+++ src/app.js
@@ -12,18 +12,40 @@ const app = () => {
     snakePosition: constants.INITIAL_SNAKE_POSITION,
     currentDirection: constants.INITIAL_DIRECTION,
     requestedDirection: null,
-    applePosition: getNewApplePosition(constants.INITIAL_SNAKE_POSITION),
-    gameState: "IDLE"
+    applePosition: null,
+    gameState: 'IDLE',
   };
 
   const getCellIndexByPosition = ([x, y]) => y * constants.GRID_WIDTH + x;
 
-  const renderField = (position) => {
+  const renderField = (snakePosition) => {
     const field = document.querySelector('#game-field');
     const cells = field.querySelectorAll('.cell');
 
-    cells.forEach((cell) => cell.classList.remove('snake'));
-    cells[getCellIndexByPosition(position)].classList.add('snake');
+    cells.forEach((cell) => {
+      cell.classList.remove('snake');
+      cell.classList.remove('snake-head');
+    });
+    
+    snakePosition.forEach((snakeSegmentPosition, index) => {
+      if (index === 0) {
+        cells[getCellIndexByPosition(snakeSegmentPosition)].classList.add(
+          'snake-head'
+        );
+      }
+
+      cells[getCellIndexByPosition(snakeSegmentPosition)].classList.add(
+        'snake'
+      );
+    });
+  };
+
+  const renderApple = (applePosition) => {
+    const field = document.querySelector('#game-field');
+    const cells = field.querySelectorAll('.cell');
+
+    cells.forEach((cell) => cell.classList.remove('apple'));
+    cells[getCellIndexByPosition(applePosition)].classList.add('apple');
   };
 
   const watchedState = new Proxy(initialState, {
@@ -35,11 +57,11 @@ const app = () => {
           console.log(`new position ${value}`);
           renderField(value);
           break;
-
+        case 'applePosition':
+          renderApple(value);
+          break;
         default:
-          throw new Error(
-            `Invalid field update attempt. Trying to update ${prop}`
-          );
+          console.log(state);
       }
 
       return true;
@@ -51,38 +73,73 @@ const app = () => {
       snakePosition,
       currentDirection,
       requestedDirection,
-      // applePosition
+      applePosition,
     } = watchedState;
-    const newDirection = getDirection(currentDirection, requestedDirection)
-    const newHeadPosition = getNewHeadPosition(snakePosition[0], newDirection)
+    const newDirection = getDirection(currentDirection, requestedDirection);
 
-    // const appleEaten = isAppleEaten(newHeadPosition, applePosition)
-    // let newBodyPosition;
-    // if (appleEaten) {
-    //   newBodyPosition = snakePosition
-    // } else {
-    //   newBodyPosition = snakePosition.slice(0, -1)
-    // }
-    const newBodyPosition = snakePosition.slice(0, -1)
+    if (newDirection !== currentDirection) {
+      watchedState.currentDirection = newDirection;
+      watchedState.requestedDirection = null;
+    }
+
+    const newHeadPosition = getNewHeadPosition(snakePosition[0], newDirection);
+
+    const appleEaten = isAppleEaten(newHeadPosition, applePosition);
+    let newBodyPosition;
+    if (appleEaten) {
+      watchedState.applePosition = getNewApplePosition(snakePosition);
+      newBodyPosition = snakePosition;
+    } else {
+      newBodyPosition = snakePosition.slice(0, -1);
+    }
     const newSnakePosition = [newHeadPosition, ...newBodyPosition];
     if (checkPositionValid(newSnakePosition)) {
-      if (newSnakePosition.length === constants.GRID_WIDTH * constants.GRID_HEIGHT) {
-        state.gameState = "GAME_WON"
-        return
+      if (
+        newSnakePosition.length ===
+        constants.GRID_WIDTH * constants.GRID_HEIGHT
+      ) {
+        watchedState.gameState = 'GAME_WON';
+        return;
       }
-      state.applePosition = getNewApplePosition(snakePosition)
+      watchedState.snakePosition = newSnakePosition;
     } else {
-      state.gameState = "GAME_OVER"
+      watchedState.gameState = 'GAME_OVER';
     }
-  }
+  };
 
   document.addEventListener('keydown', (e) => {
-    e.preventDefault();
+    const keyCode = e.code;
+    const isArrowKey = Object.keys(constants.ARROW_KEYS_MAP).includes(keyCode);
 
-    // set new direction based on the key code
+    if (keyCode === 'Space') {
+      if (watchedState.gameState === 'IDLE') {
+        if (!watchedState.applePosition) {
+          watchedState.applePosition = getNewApplePosition(
+            constants.INITIAL_SNAKE_POSITION
+          );
+        }
+        watchedState.gameState = 'PLAY';
+        return;
+      } else if (watchedState.gameState === 'PLAY') {
+        watchedState.gameState = 'IDLE';
+        return;
+      }
+    }
+
+    if (!isArrowKey) {
+      return;
+    }
+
+    e.preventDefault();
+    watchedState.requestedDirection = constants.ARROW_KEYS_MAP[keyCode];
   });
 
-  renderField(watchedState.position);
+  renderField(watchedState.snakePosition);
+  setInterval(() => {
+    if (watchedState.gameState === 'PLAY') {
+      move();
+    }
+  }, 500);
 };
 
 export default app;
